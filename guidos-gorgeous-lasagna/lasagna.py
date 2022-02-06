4da01b2a40fc1cf79f7aaf6edf36ae512a6f1a80
le-rich <richardle1997@gmail.com>
2022-02-05 21:56:12 -0800

    basics

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -1,23 +1,16 @@
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
+"""Module docstring here xD"""
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
 
-# TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
-    """Calculate the bake time remaining.
+def bake_time_remaining(time_in_oven):
+    """Returns the bake time remaining"""
+    return abs(EXPECTED_BAKE_TIME - time_in_oven)
 
-    :param elapsed_bake_time: int baking time already elapsed.
-    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
+def preparation_time_in_minutes(layers):
+    """Returns the preparation time in minutes"""
+    return layers * PREPARATION_TIME
 
-    Function that takes the actual minutes the lasagna has been in the oven as
-    an argument and returns how many minutes the lasagna still needs to bake
-    based on the `EXPECTED_BAKE_TIME`.
-    """
-
-    pass
-
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
-
-# TODO: define the 'elapsed_time_in_minutes()' function
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Returns the elapsed time in minutes given a number of layers and how much time has passed already"""
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
